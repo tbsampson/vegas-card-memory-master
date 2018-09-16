c100c6f87ccabea74e54606fb0a26d3619bdd886
Tom Sampson <tbsampson@gmail.com>
2018-09-16 15:00:47 -0500

    first version complete

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./playing-cards.json";
+import "./App.css";
+
+class App extends Component {
+    state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(`High Score: ${this.state.highscore}`);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over! \nScore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  render() { // -------------------------------------------------------------------------- render
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Vegas Card Memory Master (hint: scale brower to show 4 rows)</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.src}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
